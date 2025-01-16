d7cce34c8ea58e8619487c52e043775caaa36d36
gordonzzl <58498575+gordonzzl@users.noreply.github.com>
2025-01-16 22:03:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,13 @@ date_version=$(date +"%Y%m%d%H")
 echo $date_version > version
 
 # 为iStoreOS固件版本加上编译作者
-author="xiaomeng9597"
+author="gordonzzl"
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
 
 # 拉取我的软件包仓库
 sed -i '$a src-git xmpackage https://github.com/xiaomeng9597/openwrt-packages2.git;main' feeds.conf.default
+sed -i '$a src-git smallpack https://github.com/kenzok8/small.git;master' feeds.conf.default
+sed -i '$a src-git tailscale https://github.com/asvow/luci-app-tailscale.git;master' feeds.conf.default
+sed -i '$a src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git;master' feeds.conf.default
+
